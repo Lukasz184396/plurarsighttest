80d1e51285436e5fd6042dc7a04bf06a4aa6c765
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-27 16:40:48 +0200

    Modified wrap method to pass both tests

--- src/main/java/com/zawadalukasz/triangulation/WordWrap.java
+++ src/main/java/com/zawadalukasz/triangulation/WordWrap.java
@@ -5,9 +5,12 @@ public class WordWrap {
     public static String wrap(final String inputLine,final int lineLength) {
         final StringBuilder accumulator = new StringBuilder();
 
-        accumulator.append(inputLine, 0, lineLength);
-        accumulator.append("\n");
-        accumulator.append(inputLine, lineLength, inputLine.length());
+        accumulator.append(inputLine, 0, Math.min(inputLine.length(), lineLength));
+
+        if(inputLine.length()>lineLength) {
+            accumulator.append("\n");
+            accumulator.append(inputLine, lineLength, inputLine.length());
+        }
 
         return accumulator.toString();
     }
