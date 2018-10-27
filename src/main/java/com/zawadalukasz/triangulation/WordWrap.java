ad4e35809ac8c60ba18fe13e546fe7f731c144e3
Lukasz184396 <zawadalukasz88@gmail.com>
2018-10-27 16:46:22 +0200

    Refactored wrap method in WordWrap class

--- src/main/java/com/zawadalukasz/triangulation/WordWrap.java
+++ src/main/java/com/zawadalukasz/triangulation/WordWrap.java
@@ -2,14 +2,15 @@ package com.zawadalukasz.triangulation;
 
 public class WordWrap {
 
-    public static String wrap(final String inputLine,final int lineLength) {
+    public static String wrap(final String inputLine, final int lineLength) {
         final StringBuilder accumulator = new StringBuilder();
 
-        accumulator.append(inputLine, 0, Math.min(inputLine.length(), lineLength));
+        final int length = inputLine.length();
+        accumulator.append(inputLine, 0, Math.min(length, lineLength));
 
-        if(inputLine.length()>lineLength) {
+        if (length > lineLength) {
             accumulator.append("\n");
-            accumulator.append(inputLine, lineLength, inputLine.length());
+            accumulator.append(inputLine, lineLength, length);
         }
 
         return accumulator.toString();
